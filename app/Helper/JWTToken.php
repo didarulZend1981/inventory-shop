f9b7bc5c5a75b00e3f9b41fb48390e8b2ac768be
mohammad didarul islam <didarul1981@gmail.com>
2025-01-15 00:00:36 +0600

    verify-otp

--- app/Helper/JWTToken.php
+++ app/Helper/JWTToken.php
@@ -22,7 +22,17 @@ class JWTToken
     }
 
 
-  
+    public static function CreateTokenForSetPassword($userEmail):string{
+        $key =env('JWT_KEY');
+        $payload=[
+            'iss'=>'laravel-token',
+            'iat'=>time(),
+            'exp'=>time()+60*20,
+            'userEmail'=>$userEmail,
+            // 'userID'=>'0'
+        ];
+        return JWT::encode($payload,$key,'HS256');
+    }
 
 
     public static function VerifyToken($token):string|object
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/', function () {
 Route::post('/user-registration',[UserController::class,'UserRegistration']);
 Route::post('/user-login',[UserController::class,'UserLogin']);
 Route::post('/send-otp',[UserController::class,'SendOTPCode']);
+
+Route::post('/verify-otp',[UserController::class,'VerifyOTP']);
